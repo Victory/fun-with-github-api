c108423663af9d9c4f94ee36410b6b029eb4a014
Victory <git@dfhu.org>
2016-06-01 18:49:18 -0400

    Send a POST request

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express');
+//var request = require('request');
+var bodyParser = require('body-parser');
+
 var app = express();
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
-  res.send('Query Node');
+  res.send('<form method="POST"><input name="location"><button>Submit</button></form>');
+});
+
+app.post('/', function (req, res) {
+  console.log(req.body.location);
+  res.redirect('/');
 });
 
 app.listen(3000, function () {
