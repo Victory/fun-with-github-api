72030d037db1c393f3fa94d10a27495fc453aa3d
Victory <git@dfhu.org>
2016-06-02 18:20:29 -0400

    Query by location.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
-//var request = require('request');
+var request = require('request');
 var bodyParser = require('body-parser');
+var queryString = require("querystring");
 
 var app = express();
 
+var apiBase = "https://api.github.com";
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -14,8 +16,31 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.body.location);
-  res.redirect('/');
+  var location = req.body.location;
+  if (typeof location === "undefined") {
+    res.redirect('/');
+  }
+
+  var endpoint = apiBase + "/search/users?";
+  var query = queryString.stringify({q: "javascript", location: location});
+
+  var options = {
+    url: endpoint + query,
+    headers: {
+      'User-Agent': 'Victory'
+    }
+  };
+
+  request(options, function (err, response, body) {
+    if (!err && response.statusCode === 200) {
+      res.writeHead(200, {"Content-Type": "application/json"});
+      res.end(body);
+    } else if (err) {
+      res.send(err);
+    } else {
+      res.send(body);
+    }
+  });
 });
 
 app.listen(3000, function () {
